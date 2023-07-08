aab9b39714ab432fb6c405512bf9c05442127211
Dmytro-Lys <d.lysenko122@gmail.com>
2023-07-08 18:35:36 +0300

    fix package.json

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,8 @@
-import css from './Profile.module.css'
+import css from './Profile.module.css';
+import PropTypes from "prop-types";
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
-  const { followers, views, likes } = stats;
+  const { followers = 0, views = 0, likes = 0 } = stats;
     return (<div className={css.profile}>
     <div className={css.description}>
     <img
@@ -28,4 +29,12 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
     </li>
   </ul>
 </div>)
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired
 }
